8810fefc117c14348211bd6c76a7c5c4062d15f0
DeFox <wildmrdefox@gmail.com>
2022-10-03 19:18:21 +0700

    База/Объекты/Практика на поиск ошибок

---
+++ 1. База/3. Объекты/Поиск ошибок.js
@@ -0,0 +1,27 @@
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(obj.x);
+
+
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+
+// console.log(obj[key]);
+
+
+// let obj = {x: 1, y: 2, z: 3};
+// let sum = obj.x + obj.y + obj.z;
+
+// console.log(sum);
+
+
+// let obj = {x: 1, y: 2, z: 3};
+
+// function lengthObj (obj) {
+//   let count = 0;
+//   for(elem in obj) {
+//     count++;
+//   }
+//   return count;
+// }
+
+// console.log(lengthObj(obj))
