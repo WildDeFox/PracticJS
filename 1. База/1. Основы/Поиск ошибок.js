65dff2b0e83fab8a8127711fc3472f8a490038ee
DeFox <wildmrdefox@gmail.com>
2022-10-03 19:00:15 +0700

    База/Основы/Практика на поиск ошибок

---
+++ 1. База/1. Основы/Поиск ошибок.js
@@ -0,0 +1,58 @@
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + (+num1 + +num2));
+
+
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
+
+
+// let num = '123';
+// let sum = +num[0] + +num[1] + +num[2];
+// console.log(sum);
+
+
+// let num = '123';
+// console.log(num[0]);
+
+
+// let a = 0;
+// console.log(++a);
+
+
+// let num = 123;
+// console.log(String(num).length);
+
+
+// let a = 24 * 60 * 60;
+// console.log(a);
+
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length-1]);
+
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + Number(b);
+
+// console.log(s);
+
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+
+console.log(+aaa + +bbb + +ccc);
