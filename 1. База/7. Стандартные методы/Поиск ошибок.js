93aa6052d0d56c2b63106ea5d99e641b0fd60d33
DeFox <wildmrdefox@gmail.com>
2022-10-03 20:32:47 +0700

    База/Стандартные методы/Практика на поиск ошибок

---
+++ 1. База/7. Стандартные методы/Поиск ошибок.js
@@ -0,0 +1,53 @@
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += +digit;
+// }
+
+// console.log(sum);
+
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += +digit;
+// }
+
+// console.log(sum);
+
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += Number(digit);
+// }
+
+// console.log(sum); // почему-то выводит 5, а не 15
+
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let sum = 0;
+// for (let digit of arr) {
+// 	sum += Number(digit);
+// }
+
+// console.log(sum);
+
+
+// let num = 12345;
+// let arr = String(num).split('');
+
+// let prod = 1;
+// for (let digit of arr) {
+// 	prod *= +digit;
+// }
+
+// console.log(prod);
