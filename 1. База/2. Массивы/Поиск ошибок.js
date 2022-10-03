f44c9ca250f1ed4795964223a3dba93c08c44f35
DeFox <wildmrdefox@gmail.com>
2022-10-03 19:06:54 +0700

    База/Массивы/Практика на поиск ошибок

---
+++ 1. База/2. Массивы/Поиск ошибок.js
@@ -0,0 +1,14 @@
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length-1]);
+
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
